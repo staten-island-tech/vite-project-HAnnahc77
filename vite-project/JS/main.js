856b92b73f43175d73dadc491ec4784a76200ff5
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-11-08 13:56:07 -0500

    commit

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -17,11 +17,11 @@ albums.forEach((album) => {
     "beforeend",
     `<div class="spacing">
     <div class="card">
-        <h2 id="card-header">${album.name}</h2>
-        <img src="${album.image}" alt="Album Image">
-        <h3>${album.genre}</h3>
-        <h3>${album.language}</h3>
-        <p>${album.price}</p>
+        <h2 id="card-header" class="padding">${album.name}</h2>
+        <img src="${album.image}" alt="Album Image"class="padding">
+        <h3 class="padding">${album.genre}</h3>
+        <h3 class="padding" >${album.language}</h3>
+        <p class="padding">${album.price}</p>
       </div>
       </div>`
   );
