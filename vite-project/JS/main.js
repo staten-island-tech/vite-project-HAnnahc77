009a65235fbe8b0520d1cd87804084f155f6b295
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-11-07 21:35:20 -0500

    commit

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -13,5 +13,12 @@ DOMSelectors.button.addEventListener("click", function () {
 });
 
 albums.forEach((album) => {
-  DOMSelectors.body.insertAdjacentHTML("beforebegin");
+  DOMSelectors.body.insertAdjacentHTML("beforeend", `<div class="spacing">
+    <div class="card">
+        <h1 id="card-header">${album.name}</h1>
+        <img src="${album.image}" alt="Album Image">
+        <h2>${album.genre, album.language}</h2>
+        <p>${album.price}</p>
+      </div>
+      </div>`)
 });
